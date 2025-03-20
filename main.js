75ffa5c5cdbdea7ab56e3d7d393d2a330087d81e
deboradaddazio@yahoo.it <deboradaddazio@yahoo.it>
2025-03-20 15:19:23 +0100

    help

--- main.js
+++ main.js
@@ -4,38 +4,8 @@ import dbQuery from "./libs/dbQuery.js";
 import tableFromData from './libs/tableFromData.js'
 
 addMdToPage(`
-  ## petOwners
-  En tabell över husdjursägare.
+  ## All students
 `);
 
-let petOwners = await dbQuery('SELECT * FROM petOwners');
-tableFromData({ data: petOwners });
-
-console.log(petOwners)
-
-addMdToPage(`
-  ## pets
-  En tabell över husdjur.
-`);
-
-let pets = await dbQuery('SELECT * FROM pets');
-tableFromData({ data: pets });
-
-
-addMdToPage(`
-  ## petOwnersAndPets
-  En vy över vilka husdjursägare som äger vilka husdjur.
-  * Här kan man se vilka husdjursägare som äger vilka husdjur.
-  * Men: Husdjursägare utan husdjur visas inte.
-  * Och: Husdjur ägare utan ägare visas inte heller.
-`);
-
-addMdToPage(`
-  ## ny markdown test
-  * test lista;
-  * test lista 2;
-  
-`);
-
-let petOwnersAndPets = await dbQuery('SELECT * FROM petOwnersAndPets');
-tableFromData({ data: petOwnersAndPets });
+let students = await dbQuery('SELECT * FROM Depression_study LIMIT 50');
+tableFromData({ data: students });
