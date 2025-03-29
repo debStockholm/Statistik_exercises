63a7f13d9fad1540504a0564c8faf187db9e59b9
deboradaddazio@yahoo.it <deboradaddazio@yahoo.it>
2025-03-29 16:56:37 +0100

    none

--- main.js
+++ main.js
@@ -7,5 +7,22 @@ addMdToPage(`
   ## All students
 `);
 
-let students = await dbQuery('SELECT * FROM Depression_study LIMIT 50');
-tableFromData({ data: students });
+let students_female = await dbQuery(`select Age, count(*) as ALLFemales
+   from Depression_study
+   where gender='Female'
+   group by Age
+   order by Age Desc`);
+tableFromData({ data: students_female });
+let students_female_age_group_YES = await dbQuery(` 
+  select Age, count(*) as FemaleCountwithDepression
+  from Depression_study
+  where Depression=1 and gender='Female'
+  group by Age
+  order by FemaleCountwithDepression desc`);
+tableFromData({ data: students_female_age_group_YES });
+let students_female_age_group_NO = await dbQuery(`select Age, count(*) as FemaleCountNoDepression 
+  from Depression_study
+  where Depression=0 and gender='Female'
+  group by Age
+  order by FemaleCountNoDepression desc`);
+tableFromData({ data: students_female_age_group_NO });
